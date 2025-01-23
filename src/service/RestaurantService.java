333984a2d7e819037f5beb2ff86f84a92d8a59c4
madiy <madiyar20001234@gmail.com>
2025-01-23 13:46:41 +0500

    RestaurantService

--- src/service/RestaurantService.java
+++ src/service/RestaurantService.java
@@ -1,4 +1,30 @@
 package service;
 
+import model.Restaurant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class RestaurantService {
+    private List<Restaurant> restaurants = new ArrayList<>();
+
+    public RestaurantService() {
+        // Добавим тестовые рестораны
+        restaurants.add(new Restaurant(1, "Astana Restaurant", "Astana Center"));
+        restaurants.add(new Restaurant(2, "Almaty Diner", "Almaty Downtown"));
+    }
+//конструктор
+    public List<Restaurant> getAllRestaurants() {
+        return restaurants;
+    }
+
+    public Restaurant getRestaurantById(int id) {
+        for (Restaurant restaurant : restaurants) {
+            if (restaurant.getId() == id) {
+                return restaurant;
+            }
+        }
+        return null; // Если ресторан не найден
+    }
 }
