24ab1156cf2751785d3dd960530ed099ff239a2e
Nalatka <peacediebull1@gmail.com>
2025-01-23 13:55:07 +0500

    f

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,4 +1,25 @@
 package service;
 
+import model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserService {
+    private List<User> users = new ArrayList<>();
+
+
+    public User authenticate(String username, String password) {
+        for (User user : users) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                return user;
+            }
+        }
+        return null; // Если пользователь не найден
+    }
+
+    public void registerUser(String username, String password) {
+        int newId = users.size() + 1;
+        users.add(new User(newId, username, password));
+    }
 }
