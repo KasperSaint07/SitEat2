4c5ad6aefce1744afdeee50d22be8ae8c3d88bb0
Damir <vip.ushkempir@mail.ru>
2025-01-23 15:07:21 +0500

    database connection

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola");
+
+        String url = "jdbc:postgresql://localhost:5432/siteatdb";
+        try {
+            Connection con = DriverManager.getConnection(url, "postgres", "0000");
+            System.out.println("Connected");
+
+        } catch (SQLException e) {
+            System.out.println("Exception occurred!");
+            e.printStackTrace();
+        }
     }
 }
