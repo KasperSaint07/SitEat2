fe12aec08cc6e7daf9c2967588ab5873fbf85b10
Nalatka <peacediebull1@gmail.com>
2025-01-23 14:14:27 +0500

    f

--- src/model/Table.java
+++ src/model/Table.java
@@ -1,4 +1,19 @@
 package model;
 
 public class Table {
+    private int id;
+    private int restaurantId;
+    private boolean isAvailable;
+    public Table(int id, int restaurantId, boolean isAvailable) {
+        this.id = id;
+        this.restaurantId = restaurantId;
+        this.isAvailable = isAvailable;    }
+    public int getId() {
+        return id;    }
+    public int getRestaurantId() {
+        return restaurantId;    }
+    public boolean isAvailable() {
+        return isAvailable;    }
+    public void setAvailable(boolean available) {
+        isAvailable = available;    }
 }
