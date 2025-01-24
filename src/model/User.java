eef85d8d5965b8de4c4a7db129b301dccd7a9813
madiy <madiyar20001234@gmail.com>
2025-01-24 17:02:51 +0500

    UserModel

--- src/model/User.java
+++ src/model/User.java
@@ -1,15 +1,15 @@
 package model;
 
-import lombok.Getter;
-
-@Getter
+/**
+ * Represents a user in the system.
+ */
 public class User {
-    private final int id;
-    private String login = "";
-    private String password = "";
-    private String name = "";
-    private String surname = "";
-//    private Boolean gender = Boolean.valueOf("");
+    private int id;           // ID пользователя
+    private String login;     // Логин пользователя
+    private String password;  // Пароль пользователя
+    private String name;      // Имя пользователя
+    private String surname;   // Фамилия пользователя
+    private boolean gender;   // Пол (true - мужской, false - женский)
 
     public User(int id, String login, String password, String name, String surname, boolean gender) {
         this.id = id;
@@ -17,14 +17,55 @@ public class User {
         this.password = password;
         this.name = name;
         this.surname = surname;
-//        this.gender = gender;
+        this.gender = gender;
     }
 
-    public User(int newId, String login, String password) {
-        this.id = newId;
+    public User(int id, String login, String password) {
+        this(id, login, password, "", "", true);
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
         this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 
-    public String setPassword() {return ""; }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
 }
