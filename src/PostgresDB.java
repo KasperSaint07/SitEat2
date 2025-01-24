7083f54ef21e5b988e3f221236d8738b9a4bf233
Damir <vip.ushkempir@mail.ru>
2025-01-24 15:27:49 +0500

    Created method in PostgresDB

---
+++ src/PostgresDB.java
@@ -0,0 +1,16 @@
+import Interfaces.IDB;
+import java.sql.*;
+public class PostgresDB implements IDB {
+    public Connection getConnection() {
+        String url = "jdbc:postgresql://localhost:5432/siteatdb";
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection con = DriverManager.getConnection(url, "postgres", "0000");
+            System.out.println("Connected to database");
+            return con;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+}
