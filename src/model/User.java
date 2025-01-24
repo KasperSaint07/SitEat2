f273fdabbbb538f9572b49f73a3f17edd6f3052f
Damir <vip.ushkempir@mail.ru>
2025-01-24 11:04:14 +0500

    Pom xml added Also fixed files as UserService and User

--- src/model/User.java
+++ src/model/User.java
@@ -1,17 +1,30 @@
 package model;
+
+import lombok.Getter;
+
+@Getter
 public class User {
-    private int id;
-    private String username;
-    private String password;
-    public User(int id, String username, String password) {
-        this.id = id;        this.username = username;
-        this.password = password;    }
-    public int getId() {
-        return id;    }
-    public String getUsername() {
-        return username;    }
-    public String getPassword() {
-        return password;    }
-    public void setPassword(String password) {
-        this.password = password;    }
+    private final int id;
+    private String login = "";
+    private String password = "";
+    private String name = "";
+    private String surname = "";
+    private Boolean gender = Boolean.valueOf("");
+
+    public User(int id, String login, String password, String name, String surname, boolean gender) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+    }
+
+    public User(int newId, String login, String password) {
+        this.id = newId;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String setPassword() {return ""; }
 }
