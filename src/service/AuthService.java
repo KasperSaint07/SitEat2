3c5af24e7ea7692b3b85662fa0651a4ef75e5fad
Nalatka <peacediebull1@gmail.com>
2025-01-31 08:22:43 +0500

    f

---
+++ src/service/AuthService.java
@@ -0,0 +1,20 @@
+package service;
+import model.User;
+import model.Admin
+import repositories.AdminRepository;
+import repositories.UserRepository;
+public class AuthService {
+    private AdminRepository adminRepository;
+    private UserRepository userRepository;
+    public AuthService(AdminRepository adminRepository, UserRepository userRepository) {
+        this.adminRepository = adminRepository;
+        this.userRepository = userRepository;
+    }
+    public Admin loginAsAdmin(String username, String password){
+        return adminRepository.getAdminByCrediantals(username, password);
+    }
+    public User loginAsUser(String username, String password){
+        return userRepository.getAdminByCrediantals(username, password);
+    }
+
+}
