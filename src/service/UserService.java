087b92c883b21376a68fdb983fcd54f41c4dfd69
madiy <madiyar20001234@gmail.com>
2025-02-03 12:43:00 +0500

    Edit UserService (remove sql)

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,77 +1,26 @@
 package service;
 
 import model.User;
+import repositories.UserRepository;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
-//All User logic
 public class UserService {
-    private final Connection connection;
+    private final UserRepository userRepository;
 
-    public UserService(Connection connection) {
-        this.connection = connection;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
-    // Register a new user
-    public boolean registerUser(String login, String password, String name, String surname, boolean gender) {
-        String sql = "INSERT INTO users (login, password, name, surname, gender) VALUES (?, ?, ?, ?, ?)";
-        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setString(1, login);
-            stmt.setString(2, password);
-            stmt.setString(3, name);
-            stmt.setString(4, surname);
-            stmt.setBoolean(5, gender);
-            return stmt.executeUpdate() > 0;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
+    public boolean registerUser(String username, String password) {
+        return userRepository.addUser(username, password);
     }
 
-    // Authenticate a user
-    public User authenticate(String login, String password) {
-        String sql = "SELECT * FROM users WHERE login = ? AND password = ?";
-        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setString(1, login);
-            stmt.setString(2, password);
-            ResultSet rs = stmt.executeQuery();
-            if (rs.next()) {
-                return new User(
-                        rs.getInt("id"),
-                        rs.getString("login"),
-                        rs.getString("password"),
-                        rs.getString("name"),
-                        rs.getString("surname"),
-                        rs.getBoolean("gender")
-                );
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
+    public User getUserById(int userId) {
+        return userRepository.getUserById(userId);
     }
 
-    // Get all users
     public List<User> getAllUsers() {
-        List<User> users = new ArrayList<>();
-        String sql = "SELECT * FROM users";
-        try (Statement stmt = connection.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
-            while (rs.next()) {
-                users.add(new User(
-                        rs.getInt("id"),
-                        rs.getString("login"),
-                        rs.getString("password"),
-                        rs.getString("name"),
-                        rs.getString("surname"),
-                        rs.getBoolean("gender")
-                ));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return users;
+        return userRepository.getAllUsers();
     }
 }
