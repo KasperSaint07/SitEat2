0000aed51e242d04122af2d10f28ca3ccb855c2d
Nalatka <peacediebull1@gmail.com>
2025-02-04 14:05:54 +0500

    Update to fix bugs

--- src/service/BookingService.java
+++ src/service/BookingService.java
@@ -2,26 +2,33 @@ package service;
 
 import model.Booking;
 import repositories.BookingRepository;
+import repositories.TableRepository;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class BookingService {
     private final BookingRepository bookingRepository;
+    private final TableRepository tableRepository;
 
-    public BookingService(BookingRepository bookingRepository) {
+    public BookingService(BookingRepository bookingRepository, TableRepository tableRepository) {
         this.bookingRepository = bookingRepository;
+        this.tableRepository = tableRepository;
     }
-
     public boolean createBooking(int userId, int tableId) {
-        LocalDateTime bookingTime = LocalDateTime.now();
-        return bookingRepository.addBooking(userId, tableId, Timestamp.valueOf(bookingTime));
+        if (!tableRepository.isTableAvailable(tableId)) { // Проверка занятости
+            System.out.println("Table is already reserved!");
+            return false;
+        }
+
+        boolean success = bookingRepository.addBooking(userId, tableId, LocalDateTime.now());
+        if (success) {
+            tableRepository.updateTableAvailability(tableId, false);
+            System.out.println("Table booked successfully!");
+        }
+        return success;
     }
 
-    public List<Booking> getBookingsByRestaurant(int restaurantId) {
-        return bookingRepository.getBookingsByRestaurantId(restaurantId);
-    }
     public List<Booking> getBookingsByUserId(int userId) {
         return bookingRepository.getBookingsByUserId(userId);
     }
