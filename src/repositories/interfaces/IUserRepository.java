93f7894502642ed1b1f55e06b497e8096cb2515d
Damir <vip.ushkempir@mail.ru>
2025-02-13 00:35:51 +0500

    Comment changed

--- src/repositories/interfaces/IUserRepository.java
+++ src/repositories/interfaces/IUserRepository.java
@@ -7,5 +7,8 @@ import java.util.Optional;
 public interface IUserRepository {
     boolean createUser(User user); // Создать нового пользователя
     User authenticate(String login, String password); // Аутентификация пользователя
-    Optional<User> findByLogin(String login); // Поиск пользователя по логину (StackOverFlow посмотрите скину)
+    Optional<User> findByLogin(String login); // Поиск пользователя по логину
 }
+
+
+
